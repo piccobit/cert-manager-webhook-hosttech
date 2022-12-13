a194175e5ce67b812fb420c73a133ef7197a383f
HD Stich <hans-dieter@stich.email>
2022-12-13 09:57:46 +0100

    Use kubebuilder tools v1.24.1

--- scripts/fetch-test-binaries.sh
+++ scripts/fetch-test-binaries.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-KUBE_VERSION=1.25.0
+KUBE_VERSION=1.24.1
 
-# https://storage.googleapis.com/kubebuilder-tools
+mkdir _test
+
 curl -fsSL "https://storage.googleapis.com/kubebuilder-tools/kubebuilder-tools-${KUBE_VERSION}-$(go env GOOS)-$(go env GOARCH).tar.gz" -o kubebuilder-tools.tar.gz
-tar -zvxf kubebuilder-tools.tar.gz
+tar -C _test -xf kubebuilder-tools.tar.gz
 
 rm kubebuilder-tools.tar.gz

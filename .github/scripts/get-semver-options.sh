4bc066100e80cb6381ada51c03a712ce1ddf942f
HD Stich <hans-dieter@stich.email>
2022-12-23 14:23:38 +0100

    [PRERELEASE] [PREPATCH] Fixed typo

--- .github/scripts/get-semver-options.sh
+++ .github/scripts/get-semver-options.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bump_mode=$(git log -1 --pretty=format:'%s%n' | awk 'match($0, /\[(PREPATCH|PATCH|PREMINOR|MINOR|PREMAJOR|MAJOR)\]/, a) {printf("%s\n", tolower(a[1]))}')
-tmp_preid=$(git log -1 --pretty=format:'%s%n' | awk 'match($0, /\[(RELEASE|PRERELASE|DEBUG)\]/, a) {printf("%s\n", tolower(a[1]))}')
+tmp_preid=$(git log -1 --pretty=format:'%s%n' | awk 'match($0, /\[(RELEASE|PRERELEASE|DEBUG)\]/, a) {printf("%s\n", tolower(a[1]))}')
 
 case $tmp_preid in
 
